e97c3e32922e300641ec8276f79c07999784beb4
Sulayman Maaroufi <sulayman.maaroufi.pro@gmail.com>
2025-01-31 21:28:00 +0100

    Suppression projets et design modal finis

--- FrontEnd/javascript/modal.js
+++ FrontEnd/javascript/modal.js
@@ -1,32 +1,15 @@
-async function getWorks(filter) {
+async function getWorks() {
     let ReponseWorks = await fetch("http://localhost:5678/api/works");
     let Works = await ReponseWorks.json();
-    let ObjetsFiltered = Works.filter((work) => work.category.name === "Objets");
-    let AppartsFiltered = Works.filter((work) => work.category.name === "Appartements");
-    let HotelsRestaurantsFiltered = Works.filter((work) => work.category.name === "Hotels & restaurants");
-    //Application filtres
-    if (filter === "Tous") {
-        console.log(Works);
-        setFigure(Works);
-    }; 
+    
+    setFigure(Works);
+    setModal(Works);
 
-    if (filter === "Objets") {
-        console.log(ObjetsFiltered);
-        setFigure(ObjetsFiltered);
-    };
+    let trash = document.querySelectorAll('.overlay-image');
+    trash.forEach((item) => item.addEventListener('click', (element) => deleteWorks(element)));
+};
 
-    if (filter === "Apparts") {
-        console.log(AppartsFiltered);
-        setFigure(AppartsFiltered);
-    };
-
-    if (filter === "HotelsRestaurants") {
-        console.log(HotelsRestaurantsFiltered);
-        setFigure(HotelsRestaurantsFiltered);
-    };
-}
-
-getWorks("Tous");
+getWorks();
 
 //Création des éléments et injection dans l'HTML
 function setFigure(data) {
@@ -40,79 +23,186 @@ function setFigure(data) {
     }
 };
 
-//Comportement MODAL (ouverture et fermeture)
-let LinkModal = document.querySelector(".edit");
-let modal = null;
+//COMPORTEMENT MODAL
 
+let LinkModal = document.querySelector(".edit");
+let ButtonAdd = document.querySelector('.button-add');
+let Back = document.querySelector('.back');
+
+let modal = null;
+let modal2 = null;
+
+// Fonction pour stopper la propagation
+function stopPropagation(event) {
+    event.stopPropagation();
+};
+
+// Fonction pour attacher l'écouteur de stopPropagation à une modal donnée
+function attachStopPropagation(modalElement) {
+    let modalBox = modalElement.querySelector(".modal-box");
+    if (modalBox) {
+        modalBox.addEventListener("click", stopPropagation);
+    }
+};
+
+// Fonction d'ouverture de la première modal
 function openModal(event) {
     event.preventDefault();
     modal = document.getElementById("modal");
     modal.classList.remove("invisible");
     modal.classList.add("modal");
-    modal.removeAttribute ('aria-hidden');
-    modal.setAttribute ('aria-modal', 'true');
-    modal.addEventListener("click", function(event) {
-        closeModal(event);
-    });
-    document.querySelector(".close-modal").addEventListener("click", function(event) {
-        closeModal(event);
-    });
-    document.querySelector(".modal-box").addEventListener("click", function(event) {
-        stopPropagation(event);
-    });
+    modal.removeAttribute('aria-hidden');
+    modal.setAttribute('aria-modal', 'true');
+
+    modal.addEventListener("click", closeModal);
+    document.querySelector(".close-modal").addEventListener("click", closeModal);
+    
+    attachStopPropagation(modal);
 };
 
+LinkModal.addEventListener("click", openModal);
+
+// Fonction de fermeture de la première modal
 function closeModal(event) {
-    if (modal === null) return
+    if (modal === null) return;
     event.preventDefault();
     modal.classList.add("invisible");
     modal.classList.remove("modal");
-    modal.setAttribute ('aria-hidden', 'true');
-    modal.removeAttribute ('aria-modal');
-    modal.removeEventListener("click", function(event) {
-        closeModal(event);
-    });
-    document.querySelector(".close-modal").removeEventListener("click", function(event) {
-        closeModal(event);
-    });
-    document.querySelector(".modal-box").removeEventListener("click", function(event) {
-        stopPropagation(event);
-    });
+    modal.setAttribute('aria-hidden', 'true');
+    modal.removeAttribute('aria-modal');
+
+    modal.removeEventListener("click", closeModal);
+    document.querySelector(".close-modal").removeEventListener("click", closeModal);
+
     modal = null;
-}
+};
 
-function stopPropagation(event) {
-    event.stopPropagation();
-}
-
-LinkModal.addEventListener("click", function(event) {
-    openModal(event);
-});
-
+// Fermeture avec la touche "Escape"
 window.addEventListener('keydown', function(event) {
     if (event.key === 'Escape') {
         closeModal(event);
-    };
+        closeModal2(event);
+    }
 });
 
-//Ajout automatique des projets via fetch
-async function worksModal() {
-    let ReponseWorks = await fetch("http://localhost:5678/api/works");
-    let Works = await ReponseWorks.json();
-    setModal(Works);
-};
-worksModal();
+// Fonction d'ouverture de la deuxième modal
+function modalAdd(event) {
+    event.preventDefault();
+    closeModal(event);
+    modal2 = document.getElementById('modal2');
+    modal2.classList.remove("invisible");
+    modal2.classList.add("modal");
+    modal2.removeAttribute('aria-hidden');
+    modal2.setAttribute('aria-modal', 'true');
 
+    modal2.addEventListener("click", closeModal2);
+    document.querySelector(".close-modal").addEventListener("click", closeModal2);
+
+    attachStopPropagation(modal2);
+};
+
+ButtonAdd.addEventListener('click', modalAdd);
+
+// Fonction de fermeture de la deuxième modal
+function closeModal2(event) {
+    if (modal2 === null) return;
+    event.preventDefault();
+    modal2.classList.add("invisible");
+    modal2.classList.remove("modal");
+    modal2.setAttribute('aria-hidden', 'true');
+    modal2.removeAttribute('aria-modal');
+
+    modal2.removeEventListener("click", closeModal2);
+    document.querySelector(".close-modal").removeEventListener("click", closeModal2);
+
+    modal2 = null;
+};
+
+// Fonction pour revenir à la première modal depuis la deuxième
+function backModal(event) {
+    if (modal2 === null) return;
+    event.preventDefault();
+    openModal(event);
+    closeModal2(event);
+};
+
+Back.addEventListener('click', backModal);
+
+
+//Ajout automatique des projets via fetch
 function setModal(data) {
     const GalleryModal = document.querySelector(".works-modal");
     GalleryModal.innerHTML = "";
 
     for (let i = 0; i < data.length; i++) {
         let ImageWorks = document.createElement("figure");
-        ImageWorks.innerHTML = `<img src= ${data[i].imageUrl} alt= ${data[i].title}>`;
+        ImageWorks.classList.add("image-container"); 
+        ImageWorks.innerHTML = `
+            <img class="base-image" src="${data[i].imageUrl}" alt="${data[i].title}">
+            <i class="fa-solid fa-trash-can overlay-image" id="trash${data[i].id}"></i>
+        `;
         GalleryModal.append(ImageWorks);
     }
 };
 
-//Ajout des projets à l'aide du boutton
-let ButtonAdd = document.querySelector('.button-add');
+//Fonction suppression de projets
+async function deleteWorks(element) {
+    const id = element.target.id.replace("trash", "");
+    let DeleteApi ='http://localhost:5678/api/works/';
+    const token = sessionStorage.token;
+
+    //Appel à l'API
+    const response = await fetch(DeleteApi + id, {
+        method : 'DELETE',
+        headers : {
+            'Authorization': `Bearer ${token}`,
+        }
+    });
+
+    if (response.status === 401 || response.status === 500) {
+        console.log('Erreur lors de la suppression');
+        return;
+    }else {
+        getWorks();
+    }
+};
+
+//Fonction ajout de projets
+// async function addWorks(element) {
+//     element.preventDefault();
+
+//     let imageValue = document.querySelector('.');
+//     let titleValue = document.getElementById('title');
+//     let categoryValue = document.getElementById('categories');
+
+//     let work = {
+//         image : "",
+//         title : titleValue,
+//         category : CategoryValue
+//     };
+
+//     let AddApi = 'http://localhost:5678/api/works/';
+
+//     //Appel API
+//     const response = await fetch(AddApi {
+//         method : 'POST',
+//         headers : {
+//              "Content-Type": 'application/json' 
+//           }
+//         body : JSON.stringify(work)
+//     })
+// }
+
+//Catégories select
+
+async function selectCategories() {
+    let InputCategory = document.getElementById('categories');
+    let ResponseCategoriesApi = await fetch(['http://localhost:5678/api/categories']);
+    let CategoriesApi = await ResponseCategoriesApi.json();
+    console.log(CategoriesApi);
+
+    InputCategory.innerHTML = "";
+    
+}
+
+selectCategories();
